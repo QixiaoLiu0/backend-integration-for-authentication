05d7fdb950f13538c7de3c4a14f0c28753f28604
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-04-14 23:03:43 -0600

    feat: add protected-a

--- app/tabs/protected-a/index.tsx
+++ app/tabs/protected-a/index.tsx
@@ -1,14 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { supabase } from "@/util/supabase";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 
 const Index = () => {
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      Alert.alert("sign out failed!");
+    }
+  };
   return (
-    <View>
-      <Text>protected a page</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>protected a index</Text>
+      <Pressable onPress={handleSignOut} style={[styles.button]}>
+        <Text style={styles.buttonText}>Sign out</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default Index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { marginTop: 50 },
+  text: { fontSize: 30 },
+  button: {
+    backgroundColor: "rgb(68, 181, 252)",
+    borderRadius: 6,
+    padding: 16,
+    alignItems: "center",
+    marginTop: 28,
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+});
